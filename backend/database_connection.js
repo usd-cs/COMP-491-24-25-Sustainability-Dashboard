95f504cc85f531c9a25f9ec37c82557ca769587f
Wanda Young <wyoung@sandiego.edu>
2024-11-20 16:32:38 -0800

    backend setup for logging in user (without database connection)

---
+++ backend/database_connection.js
@@ -0,0 +1,22 @@
+import pkg from 'pg'; // Import the CommonJS module as a default
+const { Pool } = pkg; // Destructure Pool from the default import
+
+/*
+const pool = new Pool({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  port: '5434',
+  database: 'postgres',
+});
+*/ // to be determined after setting up sql database
+
+export const query = async (text, params) => {
+  try {
+    const result = await pool.query(text, params);
+    return result.rows;
+  } catch (err) {
+    console.error('Database query error:', err);
+    throw err;
+  }
+};
