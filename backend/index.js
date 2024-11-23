a2de425e316d172410527e9889e1eae193143f01
Sebastian Fernandez <sebastianfernandez@sandiego.edu>
2024-11-22 21:09:36 -0800

    Combined login_route logic into the main starting point for the back end of our project. and removed redundant file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import authRoutes from './auth/routes.js';
+
+
 const app = express();
 const PORT = 3000;
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Mount auth routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
