b90585905d13c0bf2818a9b222badab67baf0694
Wanda Young <wyoung@sandiego.edu>
2025-02-16 22:23:45 -0800

    committing changes for 4 bars, change in sql error extra comma

--- backend/tables/table_queries.js
+++ backend/tables/table_queries.js
@@ -8,6 +8,7 @@ export const get30DayEnergyTotals = async () => {
   const sqlQuery = `
     SELECT 
       AVG(co2_reduction_lbs) AS co2_reduction_lbs,
+      AVG(co2_production_lbs) AS co2_production_lbs,
       AVG(heat_rate_hhv_btu_per_kwh) AS heat_rate_hhv_btu_per_kwh
     FROM energy_daily_data
     WHERE created_at >= CURRENT_DATE - INTERVAL '30 days';
@@ -27,6 +28,10 @@ export const get30DayEnergyTotals = async () => {
 
     return {
       co2_reduction_lbs: row.co2_reduction_lbs ? parseFloat(row.co2_reduction_lbs) : 0,
+      co2_production_lbs: row.co2_production_lbs ? parseFloat(row.co2_production_lbs) : 0, 
+      //nox_reduction_lbs: row.nox_reduction_lbs ? parseFloat(row.nox_reduction_lbs) : 0,
+      //nox_production_lbs: row.nox_production_lbs ? parseFloat(row.nox_production_lbs) : 0,
+      //so2_reduction_lbs: row.so2_reduction_lbs ? parseFloat(row.so2_reduction_lbs) : 0,
       heat_rate_hhv_btu_per_kwh: row.heat_rate_hhv_btu_per_kwh ? parseFloat(row.heat_rate_hhv_btu_per_kwh) : 0
     };
   } catch (error) {
