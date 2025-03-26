05144c61a0be8ecaf98cbcb35f81d7a3b3738091
sebastianfern <sfernandez973@gmail.com>
2025-03-25 21:32:32 -0700

    Formatting fix, to remove linting error

--- cypress/e2e/ChartIntegration.cy.js
+++ cypress/e2e/ChartIntegration.cy.js
@@ -1,28 +1,25 @@
-
 describe('MainPage Charts', () => {
-    beforeEach(() => {
-      cy.visit('/main'); // Navigate to MainPage
-    });
-  
-    it('should display and click Bar Chart', () => {
-      cy.get('.visual-section:nth-child(1)').should('be.visible').click();
-    });
-  
-    it('should display and click Bubble Chart', () => {
-      cy.get('.visual-section:nth-child(2)').should('be.visible').click();
-      // Confirm data load, etc.
-    });
-  
-    it('should fetch data from /api/tables/getenergy', () => {
-      // Intercept the API call
-      cy.intercept('GET', 'http://localhost:3000/api/tables/getenergy').as('barData');
+  beforeEach(() => {
+    cy.visit('/main')
+  })
 
-      // Visit the page that triggers the axios call
-      cy.visit('/bar-chart');
+  it('should display and click Bar Chart', () => {
+    cy.get('.visual-section:nth-child(1)')
+      .should('be.visible')
+      .click()
+  })
 
-      // Wait for the request and check status
-      cy.wait('@barData')
-        .its('response.statusCode')
-        .should('be.oneOf', [200, 304]);
-    });
-  });
+  it('should display and click Bubble Chart', () => {
+    cy.get('.visual-section:nth-child(2)')
+      .should('be.visible')
+      .click()
+  })
+
+  it('should fetch data from /api/tables/getenergy', () => {
+    cy.intercept('GET', 'http://localhost:3000/api/tables/getenergy').as('barData')
+    cy.visit('/bar-chart')
+    cy.wait('@barData')
+      .its('response.statusCode')
+      .should('be.oneOf', [200, 304])
+  })
+})
