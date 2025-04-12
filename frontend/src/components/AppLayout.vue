bc3318ee8a0650db3cbc1785220dbadea027e7a0
sebastianfern <sfernandez973@gmail.com>
2025-04-11 20:11:17 -0700

    Created AppLayout component that will act as a wrapper for each page of app. Will contain Navigation menu, page background and footer.

---
+++ frontend/src/components/AppLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="background-pattern">
+      <NavBar />
+      <slot></slot> <!-- This will render the content of each page -->
+    </div>
+  </template>
+  
+  <script setup>
+  import NavBar from './NavBar.vue';
+  </script>
+  
+  <style scoped>
+  .background-pattern {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #fafafa;
+    background-image: url('https://s8kvb6qnle.execute-api.us-west-2.amazonaws.com/?hash=57e2f2c6f8c556e8de9634c71a03601a&url=https://www.sandiego.edu/admission-and-aid/undergraduate/images/campus-beauty.jpg&width=2100&webp=true');
+    opacity: 0.87;
+    z-index: -1000;
+    pointer-events: none;
+  }
+  </style>
