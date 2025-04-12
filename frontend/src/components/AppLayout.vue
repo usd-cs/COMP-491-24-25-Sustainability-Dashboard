037b6541b870bd5b361962be2f9df1b7e1c3f92a
sebastianfern <sfernandez973@gmail.com>
2025-04-11 20:56:23 -0700

    Added containers  to maintain seperation between template and app content.

--- frontend/src/components/AppLayout.vue
+++ frontend/src/components/AppLayout.vue
@@ -1,25 +1,38 @@
 <template>
-    <div class="background-pattern">
-      <NavBar />
-      <slot></slot> <!-- This will render the content of each page -->
+  <div class="app-layout">
+    <div class="background-pattern"></div>
+    <NavBar />
+    <div class="content">
+    <slot></slot>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
+<script setup>
   import NavBar from './NavBar.vue';
-  </script>
-  
-  <style scoped>
-  .background-pattern {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #fafafa;
-    background-image: url('https://s8kvb6qnle.execute-api.us-west-2.amazonaws.com/?hash=57e2f2c6f8c556e8de9634c71a03601a&url=https://www.sandiego.edu/admission-and-aid/undergraduate/images/campus-beauty.jpg&width=2100&webp=true');
-    opacity: 0.87;
-    z-index: -1000;
-    pointer-events: none;
-  }
-  </style>
+</script>
+
+<style scoped>
+.background-pattern {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fafafa;
+  background-image: url('https://s8kvb6qnle.execute-api.us-west-2.amazonaws.com/?hash=57e2f2c6f8c556e8de9634c71a03601a&url=https://www.sandiego.edu/admission-and-aid/undergraduate/images/campus-beauty.jpg&width=2100&webp=true');
+  opacity: 0.87;
+  z-index: -1;
+  pointer-events: none;
+}
+.app-layout {
+  position: relative; /* Ensure child elements are positioned relative to this container */
+  min-height: 100vh; /* Ensure the layout takes the full height of the viewport */
+}
+
+.content {
+  position: relative; /* Ensure content is above the background */
+  z-index: 1; /* Place content above the background */
+  margin-top: 71px; /* Add a margin equal to the height of the nav bar */
+}
+</style>
