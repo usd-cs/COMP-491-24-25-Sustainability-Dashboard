e029c0a6a810db3a362c6e1b4d6dfe7a5293b9e7
sebastianfern <sfernandez973@gmail.com>
2025-04-11 23:52:08 -0700

    Added Applayout to rest of nav bar links.

--- frontend/src/components/Initiatives.vue
+++ frontend/src/components/Initiatives.vue
@@ -1,7 +1,32 @@
 <template>
-    <div class="background-pattern"></div>
-    <div class="dashboard-container">
-      <!-- New Navigation Bar -->
-      <NavBar/>
-    </div>
+  <AppLayout>
+    <main class="main-content">
+    </main>
+  </AppLayout> 
 </template>
+
+<script setup>
+import { useRouter} from 'vue-router'; // Import Vue Router for navigation
+import AppLayout from './AppLayout.vue';
+</script>
+
+<style scoped>
+/* Reset styles */
+* {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+
+.main-content {
+  width: 100%;
+  max-width: 1400px;  
+  margin: 0 auto;
+  padding: 20px;       
+  box-sizing: border-box;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
