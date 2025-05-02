ef815c541fad5f7a46f7987ff8c4c8f25a8d558d
sebastianfern <sfernandez973@gmail.com>
2025-05-01 22:03:37 -0700

    Added custom property to keep current tab highlight through upload data flow.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -10,7 +10,7 @@
           <li class="navLi"><router-link to="/" class="navLink" active-class="active">Summary</router-link></li>
           <li class="navLi"><router-link to="/sources" class="navLink" active-class="active">Sources</router-link></li>
           <li class="navLi"><router-link to="/initiatives" class="navLink" active-class="active">Initiatives</router-link></li>
-          <li class="navLi"><router-link to="/login" class="navLink" active-class="active">Upload</router-link></li>
+          <li class="navLi"><router-link to="/login" class="navLink":class="{ active: isUploadRoute }">Upload</router-link></li>
         </ul>
         <!-- logout button separated -->
         <button class="logout-btn" @click="handleLogout" tabindex="0">Logout →</button>
@@ -25,9 +25,14 @@
  */
 
 import { useRouter, useRoute } from 'vue-router'; // Import Vue Router for navigation
+import { computed } from 'vue';
 
 const router = useRouter();
 const route = useRoute();
+
+const isUploadRoute = computed(() =>
+  ['/login', '/select', '/upload', '/upload-success'].includes(route.path)
+);
 
 /**
  * Handle user logout.
