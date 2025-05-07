81049fcb73db91bc886da28c09b3eb344c1a972e
sebastianfern <sfernandez973@gmail.com>
2025-05-07 14:41:45 -0700

    created protected routes, so upload routes can only be used with valid JWT.

--- backend/auth/upload/upload_routes.js
+++ backend/auth/upload/upload_routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { uploadFile, uploadAthenaFile } from './upload_controller.js';
 import upload from './uploadMiddleware.js';
+import { verifyToken } from '../authMiddleware.js';
 
 const router = Router();
 
-router.post('/file-upload', upload.single('file'), uploadFile);
-router.post('/athena-upload', upload.single('file'), uploadAthenaFile);
+router.post('/file-upload', verifyToken, upload.single('file'), uploadFile);
+router.post('/athena-upload', verifyToken, upload.single('file'), uploadAthenaFile);
 // router.post('/fetch-energy-data', fetchAndStoreEnergyData);
 
 export default router;
